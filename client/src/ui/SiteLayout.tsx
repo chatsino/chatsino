3079e7090bc4a8a685ccece4d77e3dbb8015fcf9
Outrun Colors <itsoutruncolors@gmail.com>
2022-12-06 19:10:57 -0600

    Work on SiteLayout header

--- client/src/ui/SiteLayout.tsx
+++ client/src/ui/SiteLayout.tsx
@@ -1,5 +1,10 @@
-import { MenuOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
-import { Button, ConfigProvider, Layout, theme } from "antd";
+import {
+  MenuOutlined,
+  MenuUnfoldOutlined,
+  SearchOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Button, ConfigProvider, Layout, Typography, theme, Space } from "antd";
 import { ReactNode, useState } from "react";
 import { NavigationDrawer } from "./drawers";
 
@@ -28,12 +33,35 @@ export function SiteLayout({
       }}
     >
       <Layout>
-        <Layout.Header style={{ paddingInline: 12, textAlign: "right" }}>
-          <Button
-            type="text"
-            icon={<MenuIcon style={{ color: "#f5f5f5" }} />}
-            onClick={toggleMenu}
-          />
+        <Layout.Header
+          style={{
+            paddingInline: 12,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Space>
+            <Button
+              type="text"
+              icon={<MenuIcon style={{ color: "#f5f5f5" }} />}
+              style={{ marginRight: "1rem" }}
+              onClick={toggleMenu}
+            />
+            <Typography.Title level={3} style={{ margin: 0 }}>
+              # chatsino
+            </Typography.Title>
+          </Space>
+          <Space>
+            <Button
+              type="text"
+              icon={<SearchOutlined style={{ color: "#f5f5f5" }} />}
+            />
+            <Button
+              type="text"
+              icon={<UserOutlined style={{ color: "#f5f5f5" }} />}
+            />
+          </Space>
         </Layout.Header>
         <Layout style={{ padding: 12, minHeight: "100vh" }}>{children}</Layout>
       </Layout>
