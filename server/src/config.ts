68f8cf2449a7995cb130484d3e6a0e7c432b7e01
Outrun Colors, LLC <itsoutruncolors@gmail.com>
2022-12-17 01:12:01 -0600

    Remove unused config

--- server/src/config.ts
+++ server/src/config.ts
@@ -9,20 +9,6 @@ export const JWT_SECRET = process.env.JWT_SECRET ?? "";
 export const COOKIE_SECRET = process.env.COOKIE_SECRET ?? "";
 export const TICKET_SECRET = process.env.TICKET_SECRET ?? "";
 
-// SSL
-export const SSL_KEY_PATH = path.join(
-  __dirname,
-  "..",
-  ".ssh",
-  "localhost-key.pem"
-);
-export const SSL_CERTIFICATE_PATH = path.join(
-  __dirname,
-  "..",
-  ".ssh",
-  "localhost.pem"
-);
-
 // Postgres
 export const POSTGRES_HOST = process.env.POSTGRES_HOST;
 export const POSTGRES_PORT = 5432;
