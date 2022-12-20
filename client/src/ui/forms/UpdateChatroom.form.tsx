94c5121ad1fede8067f159cdb360a35a8a3978be
Outrun Colors, LLC <itsoutruncolors@gmail.com>
2022-12-19 22:08:00 -0600

    Hide/Show Security

--- client/src/ui/forms/UpdateChatroom.form.tsx
+++ client/src/ui/forms/UpdateChatroom.form.tsx
@@ -8,9 +8,12 @@ import {
   FormInstance,
   Input,
   Row,
+  Space,
+  Switch,
   Typography,
 } from "antd";
 import { useFormFields } from "hooks";
+import { useState } from "react";
 import type { ChatroomUpdate } from "loaders";
 import { FaListAlt, FaRegListAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -27,6 +30,7 @@ interface Props {
 export type UpdateChatroomFormValues = ChatroomUpdate;
 
 export function UpdateChatroomForm({ form, chatroom, onSubmit }: Props) {
+  const [showingSecurity, setShowingSecurity] = useState(false);
   const { clearErrors, handleError } = useFormFields<UpdateChatroomFormValues>(
     form,
     "description",
@@ -76,76 +80,93 @@ export function UpdateChatroomForm({ form, chatroom, onSubmit }: Props) {
           </Form.Item>
         </Col>
       </Row>
-      <Divider orientation="left">Security</Divider>
-      <Alert
-        type="warning"
-        style={{
-          padding: "0.25rem",
-        }}
-        showIcon={true}
-        icon={
-          <LockOutlined style={{ position: "relative", top: 9, left: 16 }} />
-        }
-        description={
-          <ul style={{ margin: 0 }}>
-            <li>Chatrooms with a password are considered private.</li>
-            <li>
-              Chatrooms with anyone on a whitelist are considered private.
-            </li>
-            <li>Private chatrooms do not appear in lists.</li>
-            <li>
-              Chatrooms with users on a blacklist are still considered public.
-            </li>
-          </ul>
-        }
-      />
-      <Form.Item
-        name="password"
-        label={
-          <>
-            Password{" "}
-            <Typography.Text type="secondary" style={{ marginLeft: "0.5rem" }}>
-              <small>(optional)</small>
-            </Typography.Text>
-          </>
-        }
-      >
-        <Input type="text" placeholder="Enter a password." />
-      </Form.Item>
-      <Row gutter={24}>
-        <Col span={12}>
-          <Form.Item label="Blacklist">
-            <Link to={`/chat/${chatroom.id}/settings/whitelist`}>
-              <Button
-                block={true}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <FaListAlt /> Modify Blacklist
-              </Button>
-            </Link>
+      <Divider orientation="left" style={{ marginBottom: "1rem" }}>
+        <Space>
+          <Typography.Title level={3} style={{ margin: 0 }}>
+            Security
+          </Typography.Title>
+          <Switch checked={showingSecurity} onChange={setShowingSecurity} />
+        </Space>
+      </Divider>
+      {showingSecurity && (
+        <>
+          <Alert
+            type="info"
+            style={{
+              padding: "0.25rem",
+            }}
+            showIcon={true}
+            icon={
+              <LockOutlined
+                style={{ position: "relative", top: 9, left: 16 }}
+              />
+            }
+            description={
+              <ul style={{ margin: 0 }}>
+                <li>Chatrooms with a password are considered private.</li>
+                <li>
+                  Chatrooms with anyone on a whitelist are considered private.
+                </li>
+                <li>Private chatrooms do not appear in lists.</li>
+                <li>
+                  Chatrooms with users on a blacklist are still considered
+                  public.
+                </li>
+              </ul>
+            }
+          />
+          <Form.Item
+            name="password"
+            label={
+              <>
+                Password{" "}
+                <Typography.Text
+                  type="secondary"
+                  style={{ marginLeft: "0.5rem" }}
+                >
+                  <small>(optional)</small>
+                </Typography.Text>
+              </>
+            }
+          >
+            <Input type="text" placeholder="Enter a password." />
           </Form.Item>
-        </Col>
-        <Col span={12}>
-          <Form.Item label="Whitelist">
-            <Link to={`/chat/${chatroom.id}/settings/whitelist`}>
-              <Button
-                block={true}
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-between",
-                }}
-              >
-                <FaRegListAlt /> Modify Whitelist
-              </Button>
-            </Link>
-          </Form.Item>
-        </Col>
-      </Row>
+          <Row gutter={24}>
+            <Col span={12}>
+              <Form.Item label="Blacklist">
+                <Link to={`/chat/${chatroom.id}/settings/whitelist`}>
+                  <Button
+                    block={true}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <FaListAlt /> Modify Blacklist
+                  </Button>
+                </Link>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label="Whitelist">
+                <Link to={`/chat/${chatroom.id}/settings/whitelist`}>
+                  <Button
+                    block={true}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <FaRegListAlt /> Modify Whitelist
+                  </Button>
+                </Link>
+              </Form.Item>
+            </Col>
+          </Row>
+        </>
+      )}
     </Form>
   );
 }
