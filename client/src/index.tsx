1822718bc017497ad0bc2262f36b4387fa282a35
Outrun Colors <itsoutruncolors@gmail.com>
2022-12-20 02:13:04 -0600

    Notification config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,14 @@
+import { notification as showNotification } from "antd";
 import "antd/dist/reset.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "routes";
 import "./index.css";
+
+showNotification.config({
+  top: 55,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
